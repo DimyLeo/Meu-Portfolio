52ed22d290da8fef2efd4a814d6291027275da84
Dimy Leo <leonardoprimoc@hotmail.com>
2022-07-25 15:34:16 -0300

    Trocando arquivos de img por url host

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import IconGitHub from '../img/icon-GITHUB.png';
-import IconLinkedin from '../img/icon-LINKEDIN.png';
-import Logo from '../img/logo-pq.png';
 
 class Header extends React.Component {
   render(){
     return(
       <header>
-        <a href="https://dimyleo.github.io/Meu-Portfolio"><img id="logo" src={Logo} alt="Logo"/></a>
+        <a href="https://dimyleo.github.io/Meu-Portfolio"><img id="logo" src="https://images2.imgbox.com/ee/49/Hi1SGH3A_o.png" alt="Logo"/></a>
           <nav>
               <Link to="/Meu-Portfolio" > Sobre-min </Link>
               <Link to="/habilidades" > Habilidades </Link>
@@ -16,8 +13,8 @@ class Header extends React.Component {
               <a href="#contato">Contato</a>
           </nav>
           <div className="div-icons-redes">
-            <a href="https://github.com/DimyLeo" target="_blank" rel="noreferrer"><img className="icons-redes" src={IconGitHub} alt="Icon GitHub"/></a>
-            <a href="https://www.linkedin.com/in/leo-cavalcante-701713235/" target="_blank" rel="noreferrer"><img className="icons-redes" src={IconLinkedin} alt="Icon Linkedin"/></a>
+            <a href="https://github.com/DimyLeo" target="_blank" rel="noreferrer"><img className="icons-redes" src="https://images2.imgbox.com/2a/6e/LTwpVSQx_o.png" alt="Icon GitHub"/></a>
+            <a href="https://www.linkedin.com/in/leo-cavalcante-701713235/" target="_blank" rel="noreferrer"><img className="icons-redes" src="https://images2.imgbox.com/9c/32/2RzAcNdo_o.png" alt="Icon Linkedin"/></a>
           </div>
       </header>
     )
