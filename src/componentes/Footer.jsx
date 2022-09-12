188ae221856ac9b96535e8a74f5316badb7d25ef
DimyLeo <leonardoprimoc@hotmail.com>
2022-09-12 13:48:30 -0300

    Melhoria Header e Footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { BsPhone } from 'react-icons/bs';
+import { MdAttachEmail } from 'react-icons/md';
 import { Link } from "react-router-dom";
 import MyContext from "../context/MyContext";
 import './Footer.css';
@@ -7,17 +9,17 @@ function Footer() {
   const {theme} = useContext(MyContext);
 
     return(
-      <footer className={theme ? 'dark-footer' : 'light-header-footer'}>
+      <footer className={theme ? 'dark-footer' : 'light-footer'}>
         <nav>
           <Link className={theme ? 'dark-header' : 'links-light'} to="/Meu-Portfolio" > Sobre-min </Link>
           <Link className={theme ? 'dark-header' : 'links-light'} to="/habilidades" > Habilidades </Link>
           <Link className={theme ? 'dark-header' : 'links-light'} to="/projetos" > Projetos </Link>
           <Link className={theme ? 'dark-header' : 'links-light'} to="/contato">Contato</Link>
         </nav>
-        <div>
+        <div className="contatos-footer">
           <h2>Contatos</h2>
-          <p>Celular: (67)999094613</p>
-          <p>Email: leonardoprimoc@hotmail.com</p>
+            <span className="itens-contatos"><BsPhone className="icons-footer" /><p>(67)999094613</p></span>
+            <span className="itens-contatos"><MdAttachEmail className="icons-footer" /><p>leonardoprimoc@hotmail.com</p></span>
         </div>
         <p id={theme ? 'copy' : 'copy-light'}>© Copyright DimyLeo - 2022</p>
       </footer>
