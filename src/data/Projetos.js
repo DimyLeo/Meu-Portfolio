fa9a10e3b5cc4a31b7d0505ea3ed62dd29979ddf
DimyLeo <leonardoprimoc@hotmail.com>
2022-09-14 11:40:59 -0300

    Add Map projects

---
+++ src/data/Projetos.js
@@ -0,0 +1,45 @@
+const projetos = [
+
+  {
+    id: 1,
+    title: 'Projeto App de Receitas',
+    describe: 'Esse é o um Projeto em grupo feito em React com ContextAPI, avaliando a criação de um app de Receitas que instrui e ensina receitas, é tambem avaliado as habilidades de trabalhar em grupo.',
+    link: 'https://dimyleo.github.io/Projeto-App-de-Receitas/',
+    img: 'https://images2.imgbox.com/db/23/4hQiJBMm_o.jpg'
+  },
+
+  {
+    id: 2,
+    title: 'Projeto Trivia',
+    describe: 'Esse é um projeto em grupo onde foi feito um site onde você pode jogar Trivia um jogo de perguntas e respostas, clique no link abaixo para testar.',
+    link: 'https://dimyleo.github.io/Projeto-Trivia/',
+    img: 'https://images2.imgbox.com/63/9a/jUSThz0Y_o.jpg'
+  },
+
+  {
+    id: 3,
+    title: 'Projeto Tryunfo',
+    describe: 'Esse projeto é um site onde você pode criar cards personalizados, clique no link abaixo para testar.',
+    link: 'https://dimyleo.github.io/Projeto-Tryunfo-Cards-Personalizaveis/',
+    img: 'https://images2.imgbox.com/7a/32/XE9SfU0d_o.jpg'
+  },
+
+  {
+    id: 4,
+    title: 'Projeto RTL no Pokedex',
+    describe: 'Esse projeto é um site onde você pode visualizar as Pokedex e tem o projetos com tests em React Testing Library, clique no link abaixo para testar.',
+    link: 'https://dimyleo.github.io/Projeto-RTL-com-Pokedex/',
+    img: 'https://images2.imgbox.com/5d/fc/nzGKCbC9_o.jpg'
+  },
+
+  {
+    id: 5,
+    title: 'Projeto Trybewarts Responsivo',
+    describe: 'Esse projeto é um site de um formulario escolar responsivo e validado, clique no link abaixo para testar.',
+    link: 'https://dimyleo.github.io/Projeto-Trybewarts-Responsivo/',
+    img: 'https://images2.imgbox.com/24/5a/zpgTgHEP_o.jpg'
+  }
+
+]
+
+ export default projetos;
