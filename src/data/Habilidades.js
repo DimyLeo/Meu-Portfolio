9acb1789321212500289bc324f2729e1911fbac1
DimyLeo <leonardoprimoc@hotmail.com>
2022-09-17 10:53:26 -0300

    Aplicando Map em habilidades

--- src/data/Habilidades.js
+++ src/data/Habilidades.js
@@ -1,79 +1,83 @@
+import { BsFillBootstrapFill } from 'react-icons/bs';
+import { FaGitSquare, FaNode, FaReact } from 'react-icons/fa';
+import { SiCss3, SiHtml5, SiJavascript, SiJest, SiReactrouter, SiRedux, SiTestinglibrary } from 'react-icons/si';
+
 const habilidades = [
 
   {
     id: 1,
     h3: 'JavaScript',
-    iconReact: 'SiJavascript',
+    iconReact: <SiJavascript className='icons-habilidades' />,
     className: 'div-icon1'
   },
 
   {
     id: 2,
     h3: 'React',
-    iconReact: 'FaReact',
+    iconReact: <FaReact className='icons-habilidades' />,
     className: 'div-icon2'
   },
 
   {
     id: 3,
     h3: 'Redux',
-    iconReact: 'SiRedux',
+    iconReact: <SiRedux className='icons-habilidades' />,
     className: 'div-icon1'
   },
 
   {
     id: 4,
     h3: 'React Router',
-    iconReact: 'SiReactrouter',
+    iconReact: <SiReactrouter className='icons-habilidades' />,
     className: 'div-icon2'
   },
 
   {
     id: 5,
     h3: 'React Testing',
-    iconReact: 'SiTestinglibrary',
+    iconReact: <SiTestinglibrary className='icons-habilidades' />,
     className: 'div-icon1'
   },
 
   {
     id: 6,
     h3: 'Jest Tests',
-    iconReact: 'SiJest',
+    iconReact: <SiJest className='icons-habilidades' />,
     className: 'div-icon2'
   },
 
   {
     id: 7,
     h3: 'Node JS',
-    iconReact: 'FaNode',
+    iconReact: <FaNode className='icons-habilidades' />,
     className: 'div-icon1'
   },
 
   {
     id: 8,
     h3: 'HTML 5',
-    iconReact: 'SiHtml5',
+    iconReact: <SiHtml5 className='icons-habilidades' />,
     className: 'div-icon2'
   },
 
   {
     id: 9,
     h3: 'CSS 3',
-    iconReact: 'SiCss3',
+    iconReact: <SiCss3 className='icons-habilidades' />,
     className: 'div-icon1'
   },
 
   {
     id: 10,
     h3: 'Bootstrap',
-    iconReact: 'BsFillBootstrapFill',
+    iconReact: <BsFillBootstrapFill className='icons-habilidades' />,
     className: 'div-icon2'
   },
 
   {
     id: 11,
     h3: 'Git',
-    iconReact: 'FaGitSquare',
+    iconReact: <FaGitSquare className='icons-habilidades' />,
     className: 'div-icon1'
   }
 
--- src/pages/Habilidades.jsx
+++ src/pages/Habilidades.jsx
@@ -1,7 +1,5 @@
-import { BsFillBootstrapFill } from 'react-icons/bs';
-import { FaGitSquare, FaNode, FaReact } from 'react-icons/fa';
-import { SiCss3, SiHtml5, SiJavascript, SiJest, SiReactrouter, SiRedux, SiTestinglibrary } from 'react-icons/si';
 import Fade from 'react-reveal/Fade';
+import habilidades from '../data/Habilidades';
 import './Habilidades.css';
 
 function Habilidades() {
@@ -20,64 +18,20 @@ function Habilidades() {
             <p>Esses são algumas tecnologias que tenho utilizado para o Desenvolvimento Web e ultilizei para construir este site sendo elas <b>HTML, CSS, JavaScript, React, Git...</b> E estou estudando para me tornar um Desenvolvedor Web FullStack.</p>
           </div>
         </Fade>
-        <Fade bottom>
-          <div className="div-icons-habilidades">
-            <div className='div-icons1'>
-              <h3>JavaScript</h3>
-              <SiJavascript className="icons-habilidades" />
-            </div>
 
-            <div className='div-icons2'>
-              <h3>React</h3>
-              <FaReact className="icons-habilidades" />
+        {habilidades.map((index) => {
+          return (
+            <div className="div-icons-habilidades">
+              <Fade bottom>
+                <div key={index.id} className={index.className}>
+                  <h3>{index.h3}</h3>
+                  {index.iconReact}
+                </div>
+              </Fade>
             </div>
+          )
+        })}
 
-            <div className='div-icons1'>
-              <h3>Redux</h3>
-                <SiRedux className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons2'>
-              <h3>React Router</h3>
-              <SiReactrouter className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons1'>
-              <h3>React Testing</h3>
-              <SiTestinglibrary className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons2'>
-              <h3>Jest Tests</h3>
-              <SiJest className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons1'>
-              <h3>Node JS</h3>
-              <FaNode className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons2'>
-              <h3>HTML 5</h3>
-              <SiHtml5 className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons1'>
-            <h3>CSS 3</h3>
-            <SiCss3 className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons2'>
-              <h3>Bootstrap</h3>
-              <BsFillBootstrapFill className="icons-habilidades" />
-            </div>
-
-            <div className='div-icons1'>
-              <h3>Git</h3>
-              <FaGitSquare className="icons-habilidades" />
-            </div>
-          </div>
-          </Fade>
           <img className="left-raio" src="https://images2.imgbox.com/ab/ab/lweyN9zP_o.png" alt="left-raio" />
         </div>
         <button onClick={handleClick} className='div-bottom-top-img back-top-h'>
